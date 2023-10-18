a863fc1b2d2189ca088cc3c3a713ba0a46b967a5
MatrocLeMatroc <119348896+MatrocLeMatroc@users.noreply.github.com>
2023-10-18 14:10:20 -0400

    Progression dans la la view

--- app/src/main/java/ca/csfoy/appliexam/mc/AppliExamMC.java
+++ app/src/main/java/ca/csfoy/appliexam/mc/AppliExamMC.java
@@ -5,10 +5,15 @@ import ca.csfoy.appliexam.view.AppliExamView;
 public class AppliExamMC {
 
     private AppliExamView appliExamView;
-
-
+    private Question question;
 
     public AppliExamMC(AppliExamView appliExamView){
         this.appliExamView = appliExamView;
     }
+
+    public void initQuestion() {
+        this.question = new Question();
+    }
+
+    
 }
--- app/src/main/java/ca/csfoy/appliexam/view/MainActivity.java
+++ app/src/main/java/ca/csfoy/appliexam/view/MainActivity.java
@@ -4,18 +4,50 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.util.List;
 
 import ca.csfoy.appliexam.R;
+import ca.csfoy.appliexam.mc.AppliExamMC;
 
 public class MainActivity extends AppCompatActivity implements AppliExamView{
 
+    private String txtQuestion;
+    private RadioGroup rg;
+    private Button btnSubmit;
+    private Button btnNext;
+
+    private AppliExamMC mc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.txtQuestion = findViewById(R.id.txtQuestion);
+        this.rg = findViewById(R.id.radioGroup1);
+        this.btnSubmit = findViewById(R.id.btnSubmit);
+        this.btnNext = findViewById(R.id.btnNext);
+
+        this.mc = new AppliExamMC(this);
+
+        RadioButton button;
+        for(int i = 0; i < 3; i++) {
+            button = new RadioButton(this);
+            button.setText("Button " + i);
+            this.rg.addView(button);
+        }
+
+        this.update();
+        btnSubmit.setOnClickListener((view) -> {
+            this.onSubmitAnswer();
+        });
+        btnNext.setOnClickListener((view) -> {
+            this.getToNextQuestion();
+        });
     }
 
     @Override
@@ -30,6 +62,14 @@ public class MainActivity extends AppCompatActivity implements AppliExamView{
 
     @Override
     public void update() {
+
+    }
+
+    private void onSubmitAnswer() {
+
+    }
+
+    private void getToNextQuestion() {
 
     }
 }
